cfc58e96100264d934b6dfdbe5f063c13f795428
Sean Kellogg <skellogg@articulatedman.com>
2009-02-28 15:44:10 -0800

    load ExtJS conditionally on hardcoded category ID

--- extjs-loader.php
+++ extjs-loader.php
@@ -16,20 +16,45 @@ class ExtJSLoader {
   function ExtJSLoader() {
 
     $this->pluginurl = apply_filters( 'extjsloader_url', $wpurl . '/wp-content/plugins/extjs-loader/' );
+    $this->load_on_cat_id = 10;
 
+    add_action( 'wp_head', array(&$this, 'MatchCategory'), 1000 );
     add_action( 'wp_head', array(&$this, 'AddStylesheet'), 1000 );
     add_action( 'wp_head', array(&$this, 'AddJavascript'), 1000 );
 
   }
 
+  function MatchCategory(){
+
+    $cats = get_the_category( the_ID() );
+
+    foreach( $cats as $cat ){
+      if ($cat->term_id == $this->load_on_cat_id ){
+        $this->load_extjs = true;
+        return;
+      }
+    }
+
+    $this->load_extjs = false;
+
+  }
+
   // Stick the stylesheet in the header
   function AddStylesheet() {
-    echo '  <link type="text/css" rel="stylesheet" href="' . $this->pluginurl . 'extjs/resources/css/ext-all.css"></link>' . "\n";
+
+    if ( $this->load_extjs ){
+      echo '  <link type="text/css" rel="stylesheet" href="' . $this->pluginurl . 'extjs/resources/css/ext-all.css"></link>' . "\n";
+    }
+
   }
 
   function AddJavascript() {
-    echo '  <script type="text/javascript" src="' . $this->pluginurl . 'extjs/adapter/ext/ext-base.js"></script>' . "\n";
-    echo '  <script type="text/javascript" src="' . $this->pluginurl . 'extjs/ext-all.js"></script>' . "\n";
+
+    if ( $this->load_extjs ){
+      echo '  <script type="text/javascript" src="' . $this->pluginurl . 'extjs/adapter/ext/ext-base.js"></script>' . "\n";
+      echo '  <script type="text/javascript" src="' . $this->pluginurl . 'extjs/ext-all.js"></script>' . "\n";
+    }
+
   }
 
 }
